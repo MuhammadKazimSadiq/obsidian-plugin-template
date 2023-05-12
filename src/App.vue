a9b510fbd835ca7a9c927e4635e716f2257174f4
Muhammad Kazim <md.kazimsadiq@gmail.com>
2023-05-12 12:42:00 +0430

    project setup with vite + vue

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>Obsidian Plugin</h1>
+</template>
+
+<script setup>
+const props = defineProps({
+  app: {
+    type: Object,
+    required: true,
+  },
+});
+</script>
+
+<style></style>
